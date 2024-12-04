a47ca576482141671afc85e2861d6c28b55d666d
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-04 19:43:48 +0600

    Math object

---
+++ index.js
@@ -0,0 +1,38 @@
+//absulate method
+var num=-19;
+console.log(
+    Math.abs(num)
+)
+
+//ceil method
+var num=6.7
+console.log(
+    Math.ceil(num)
+)
+
+//floor method
+var num=8.7
+console.log(
+    Math.floor(num)
+)
+
+//Max Method
+console.log(
+    Math.max(23,1,5,18,99)
+)
+
+//min Method
+console.log(
+    Math.min(23,1,5,18,99)
+)
+
+//random Method
+console.log(
+    Math.random()
+)
+
+//random Method
+var num=7.6;
+console.log(
+    Math.round(num)
+)
